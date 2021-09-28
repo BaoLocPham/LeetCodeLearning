4e87a9c5f9ffa8f4e45178e79da71fcc64ea97d6
Bao Loc Pham <67360122+BaoLocPham@users.noreply.github.com>
2021-09-28 10:26:22 +0700

    Update 231. Power of two.py

--- Algorithm I/Day 13 - Bit Manipulation/231. Power of Two.py
+++ Algorithm I/Day 13 - Bit Manipulation/231. Power of Two.py
@@ -4,7 +4,7 @@ class Solution:
         # Concept: because power of two meaning the 2^0 bits is zero
         # 1000 = 8
         # &
-        # 0101 = 7
+        # 0111 = 7
         # -----
         # 0000 
         if n<=0:
